0e506a299009663c27e8d37c1fa750c5545e88f3
Lucas-Cold <lucas.coldreturn@gmail.com>
2023-07-27 14:06:44 -0300

    Context Pokedex ver1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importe o componente Routes
+import Favorites from './Favorites';
+import Pokdex from './Pokdex';
+import FavoritesProvider from './FavoritesContext';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+     <FavoritesProvider>
+      <Routes> {}
+        <Route path="/" element={<Pokdex />} /> {}
+        <Route path="/favorites" element={<Favorites />} /> {}
+      </Routes>
+     </FavoritesProvider>
+    </Router>
+);
 }
-
 export default App;
